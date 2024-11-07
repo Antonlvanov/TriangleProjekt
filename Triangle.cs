05c137195a5e737ad9aa34a2c99c1cdac98678bc
Antonlvanov <156616115+Antonlvanov@users.noreply.github.com>
2024-11-07 09:59:58 +0200

    Add files via upload

--- Triangle.cs
+++ Triangle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Triangle
 {
@@ -10,16 +11,19 @@ namespace Triangle
     {
         public double a, b, c;
         private double _angleA, _angleB, _angleC;
+        public double _perimeter, _surface;
         public TriangleType Type { get; private set; }
         public Triangle(double A, double B, double C) {
+
             a = A; b = B; c = C;
 
-            _angleA = CalculateAngle(A, B, C);
-            _angleB = CalculateAngle(B, A, C);
-            _angleC = CalculateAngle(C, A, B);
-
-            if (ExistTriangle) 
+            if (ExistTriangle)
             {
+                _angleA = CalculateAngle(A, B, C);
+                _angleB = CalculateAngle(B, A, C);
+                _angleC = CalculateAngle(C, A, B);
+                FindPerimeter();
+                FindSurface();
                 DetermineTriangleType();
             }
             else
@@ -27,18 +31,14 @@ namespace Triangle
                 Type = TriangleType.NotExist; 
             }
         }
-        public double Perimeter()
+        public void FindPerimeter()
         {
-            double p = 0;
-            p = a + b + c;
-            return p;
+            _perimeter = a + b + c;
         }
-        public double Surface()
+        public void FindSurface()
         {
-            double s, p = 0;
-            p = (a + b + c) / 2;
-            s = Math.Sqrt((p * (p - a) * (p-b) * (p-c)));
-            return s;
+            double p = _perimeter / 2;
+            _surface = Math.Sqrt((p * (p - a) * (p-b) * (p-c)));
         }
 
         public double CalculateAngle(double side1, double side2, double oppositeSide)
@@ -68,8 +68,58 @@ namespace Triangle
 
             Type = TriangleType.Scalene;
         }
+        public void SaveDataXML()
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                NewLineOnAttributes = false
+            };
+            string filePath = @"..\..\data.xml";
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    using (XmlWriter writer = XmlWriter.Create(filePath, settings))
+                    {
+                        writer.WriteStartDocument();
+                        writer.WriteStartElement("Triangles");
+                        writer.WriteEndElement();
+                        writer.WriteEndDocument();
+                    }
+                }
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filePath);
+
+                XmlElement triangleElem = doc.CreateElement("Triangle");
+
+                var fieldValues = this.GetType()
+                     .GetFields()
+                     .Select(field => field.GetValue(this))
+                     .ToList();
+
+                string[] fieldNames = { "Side a:", "Side b:", "Side c:", "Angle A:", "Angle B:", "Angle C:", "Perimeter:", "Surface:", "Type:" };
+
+                for (int i = 0; i < fieldValues.Count && i < fieldNames.Length; i++)
+                {
+                    XmlElement fieldElement = doc.CreateElement(fieldNames[i].Replace(" ", ""));
+                    fieldElement.InnerText = fieldNames[i] + " " + fieldValues[i]?.ToString();
+                    triangleElem.AppendChild(fieldElement);
+                }
+
+                doc.DocumentElement.AppendChild(triangleElem);
+                doc.Save(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to save file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
 
+        // properties
         public bool IsEquilateral
         {
             get
@@ -120,7 +170,6 @@ namespace Triangle
             }
         }
 
-        // properties
         public double AngleA
         {
             get
